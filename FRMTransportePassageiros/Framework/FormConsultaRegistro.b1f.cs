d051e9b8ca139013ec672dd2c80295558a3bb06c
fernando.muscalu <fernando.muscalu@itmil.com.br>
2024-07-03 22:11:23 -0300

    atualizaão do repositório

--- FRMTransportePassageiros/Framework/FormConsultaRegistro.b1f.cs
+++ FRMTransportePassageiros/Framework/FormConsultaRegistro.b1f.cs
@@ -20,12 +20,13 @@ namespace FRMTransportePassageiros.Framework
         /// </summary>
         public override void OnInitializeComponent()
         {
+            //this.UIAPIRawForm.DataSources.DataTables.Add("DTConsul");
             this.lblProcurar = ((SAPbouiCOM.StaticText)(this.GetItem("lblPesq").Specific));
             this.txtProcurar = ((SAPbouiCOM.EditText)(this.GetItem("Item_1").Specific));
             this.mtxConsulta = ((SAPbouiCOM.Matrix)(this.GetItem("mtxCons1").Specific));
             this.btnSelecionar = ((SAPbouiCOM.Button)(this.GetItem("btnSelec").Specific));
-            this.btnCancelar = ((SAPbouiCOM.Button)(this.GetItem("btnCanc1").Specific));            
-
+            this.btnCancelar = ((SAPbouiCOM.Button)(this.GetItem("btnCanc1").Specific));
+            //this.dtConsulta = this.UIAPIRawForm.DataSources.DataTables.Item("DTConsul"); 
             this.OnCustomInitialize();
 
         }
@@ -35,11 +36,13 @@ namespace FRMTransportePassageiros.Framework
         /// </summary>
         public override void OnInitializeFormEvents()
         {
-            this.LoadAfter += new LoadAfterHandler(this.Form_LoadAfter);
+           //this.LoadAfter += new LoadAfterHandler(this.Form_LoadAfter);
         }
-
+        //Este método é essencial para inicialização dos componentes (ou itens de form)
+        //que 
         private void OnCustomInitialize()
         {
+            this.Form_LoadAfter();
         }
 
         public void SetFilter(string[,] filtros)
@@ -57,18 +60,18 @@ namespace FRMTransportePassageiros.Framework
                     this.SetFilter(filtros);
 
                 buildQuery.Append("SELECT");
-                buildQuery.Append("T0.\"Code\",");
-                buildQuery.Append("T0.\"Name\",");
+                buildQuery.Append(" T0.\"Code\",");
+                buildQuery.Append(" T0.\"Name\" ");
                 buildQuery.Append("FROM");
-                buildQuery.AppendFormat("\"{0}\" T0",_userTable);
+                buildQuery.AppendFormat(" \"{0}\" T0",_userTable);
                 
                 if ( filter != null )
                 {
-                    buildQuery.Append("WHERE");
+                    buildQuery.Append("WHERE ");
 
                     for (int ind = 0; ind <= filter.Length -1; ind++)
                     {
-                        buildQuery.AppendFormat("T0.\"{0}\" = '{1}' ", filter[ind,1], filter[ind,2]);
+                        buildQuery.AppendFormat(" T0.\"{0}\" = '{1}' ", filter[ind,1], filter[ind,2]);
 
                         if (ind < filter.Length - 1)
                             buildQuery.Append(" AND ");
@@ -84,10 +87,10 @@ namespace FRMTransportePassageiros.Framework
         {
             try
             {
-                this.UIAPIRawForm.DataSources.DataTables.Item("DTCon001").ExecuteQuery(this.query);
-
-                mtxConsulta.Columns.Item("cCodCon").DataBind.Bind("DTCon001", "Code");
-                mtxConsulta.Columns.Item("cNameCon").DataBind.Bind("DTCon001", "Name");
+                this.UIAPIRawForm.DataSources.DataTables.Item("DTCon1").ExecuteQuery(this.query);
+                //this.dtConsulta.ExecuteQuery(this.query);
+                mtxConsulta.Columns.Item("cCodCon").DataBind.Bind("DTCon1", "Code");
+                mtxConsulta.Columns.Item("cNameCon").DataBind.Bind("DTCon1", "Name");
 
                 mtxConsulta.AutoResizeColumns();
 
@@ -109,11 +112,12 @@ namespace FRMTransportePassageiros.Framework
         private SAPbouiCOM.Matrix mtxConsulta;
         private SAPbouiCOM.Button btnSelecionar;
         private SAPbouiCOM.Button btnCancelar;
+        private SAPbouiCOM.DataTable dtConsulta = null;
         private string _userTable;
         private string query;
         private string[,] filter = null;
 
-        private void Form_LoadAfter(SAPbouiCOM.SBOItemEventArg pVal)
+        private void Form_LoadAfter()//(SAPbouiCOM.SBOItemEventArg pVal)
         {
             this._userTable = Tools.TableLookUp;
             this.SetQuery(true);
